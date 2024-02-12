d5e1513a3c5aed819eac3c9f7bae18dd7207c825
D4rkisek <maruf.f@hotmail.com>
2024-02-12 18:44:34 +0000

    Add project files.

---
+++ gymLog/gymLog/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using gymLog.Models;
+using gymLog.ViewModels;
+
+namespace gymLog.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
